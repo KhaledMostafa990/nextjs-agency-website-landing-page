b32f3bcdabac25ea60a662b06335e6a6bd93288f
khaled farghly <khaledfarghly9991@gmail.com>
2022-10-01 23:56:12 +0200

    enhance feature quilty toggle state and complete pricing section

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
+import Head from 'next/head';
 import CoreFeatureSection from '../components/CoreFeatureSection';
 import FeaturesQualitySection from '../components/FeaturesQualitySection';
 import Hero from '../components/Hero';
+import Pricing from '../components/Pricing';
 import SupportSection from '../components/SupportSection';
 
 export default function Home({ data }) {
   return (
     <>
+      <Head>
+        <title>Agency landing page</title>
+      </Head>
+
       <Hero />
       <SupportSection />
       <CoreFeatureSection accordionData={data.accordion} />
       <FeaturesQualitySection featureQuality={data.featureQulity} />
+      <Pricing pricing={data.pricing} />
     </>
   );
 }
@@ -66,6 +73,121 @@ export async function getStaticProps() {
         desc: 'Build an incredible workplace and grow your business with Gusto’s all-in-one platform with amazing contents. Get your tests delivered at let home collect sample from the victory of the supplies design system.',
       },
     ],
+    pricing: {
+      monthly: [
+        {
+          packName: 'Startup Pack',
+          price: '$25.99/mo',
+          audience: 'For the startup team who work with new come data stack',
+          benefits: [
+            {
+              name: 'Ultimate access to all course, exercises and assessments',
+              avaliable: true,
+            },
+            {
+              name: 'Free acess for all kind of exercise corrections with downloads.',
+              avaliable: true,
+            },
+            {
+              name: 'Total assessment corrections with free download access system',
+              avaliable: true,
+            },
+            {
+              name: 'Unlimited download of courses on the mobile app contents',
+              avaliable: false,
+            },
+            {
+              name: 'Download and print courses and exercises in PDF',
+              avaliable: false,
+            },
+          ],
+        },
+        {
+          packName: 'Premium Pack',
+          price: '$49.99/mo',
+          audience: 'For the Pro users who work with modern data stack',
+          benefits: [
+            {
+              name: 'Ultimate access to all course, exercises and assessments',
+              avaliable: true,
+            },
+            {
+              name: 'Free acess for all kind of exercise corrections with downloads.',
+              avaliable: true,
+            },
+            {
+              name: 'Total assessment corrections with free download access system',
+              avaliable: true,
+            },
+            {
+              name: 'Unlimited download of courses on the mobile app contents',
+              avaliable: true,
+            },
+            {
+              name: 'Download and print courses and exercises in PDF',
+              avaliable: true,
+            },
+          ],
+        },
+      ],
+      annual: [
+        {
+          packName: 'Startup Pack',
+          price: '$249.99/mo',
+          audience: 'For the startup team who work with new come data stack',
+          benefits: [
+            {
+              name: 'Ultimate access to all course, exercises and assessments',
+              avaliable: true,
+            },
+            {
+              name: 'Free acess for all kind of exercise corrections with downloads.',
+              avaliable: true,
+            },
+            {
+              name: 'Total assessment corrections with free download access system',
+              avaliable: true,
+            },
+            {
+              name: 'Unlimited download of courses on the mobile app contents',
+              avaliable: false,
+            },
+            {
+              name: 'Download and print courses and exercises in PDF',
+              avaliable: false,
+            },
+          ],
+        },
+        {
+          packName: 'Premium Pack',
+          price: '$309.99/mo',
+
+          audience: 'For the Pro users who work with modern data stack',
+          benefits: [
+            {
+              name: 'Ultimate access to all course, exercises and assessments',
+              avaliable: true,
+            },
+            {
+              name: 'Free acess for all kind of exercise corrections with downloads.',
+              avaliable: true,
+            },
+            {
+              name: 'Total assessment corrections with free download access system',
+              avaliable: true,
+            },
+            {
+              name: 'Unlimited download of courses on the mobile app contents',
+              avaliable: true,
+            },
+            {
+              name: 'Download and print courses and exercises in PDF',
+              avaliable: true,
+            },
+          ],
+        },
+      ],
+    },
   };
 
   return {
